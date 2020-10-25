958e5e394d35ceb6b580bd561b80049652dc2be0
Altay Batuhan <45828884+altarrok@users.noreply.github.com>
2020-10-25 11:29:40 -0700

    Implemented Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,28 @@
+import styled from "@emotion/styled"
 import React from "react"
+import COLORPALETTE from "../COLORPALETTE.json"
 
 const Footer = () => {
 
+  const FooterDiv = styled.div`
+    min-height: 5vh;
+    position: relative;
+    background-color: ${COLORPALETTE.darkBlack}
+  `
+
+  const StyledSpan = styled.span`
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: ${COLORPALETTE.darkWhite}
+  `
+
   return (
-    <>
-    Footer
-    </>
+    <FooterDiv>
+      <StyledSpan>Made by Altay Batuhan. Using Gatsby and hosted with Netlify.</StyledSpan>
+    </FooterDiv>
   )
 }
 
