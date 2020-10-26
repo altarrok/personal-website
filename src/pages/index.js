b14333f198e284ec8bd73156754523e02bf6b2a6
Altay Batuhan <45828884+altarrok@users.noreply.github.com>
2020-10-26 00:09:12 -0700

    Importing Fonts with Helmet instead of inside CSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -12,6 +13,9 @@ import Greveyard from "../views/greveyard"
 
 const IndexPage = () => (
   <Layout>
+    <Helmet>
+      <link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
+    </Helmet>
     <SEO title="Home" />
     <Entrance />
     <Bio />
