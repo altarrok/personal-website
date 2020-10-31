ff915a1551603e1bb176eb933ed38b0dfc5cb982
Altay Batuhan <45828884+altarrok@users.noreply.github.com>
2020-10-31 13:34:34 -0700

    Added ProjectSubContent Component

--- src/views/projects.js
+++ src/views/projects.js
@@ -6,6 +6,7 @@ import { AiOutlineFundProjectionScreen } from "react-icons/ai";
 
 import Container from "../components/container"
 import Card from "../components/card"
+import ProjectsSubContent from "../components/projectsSubContent";
 
 const textureId = "Projects";
 const Texture =
@@ -22,6 +23,11 @@ const iconFactory = (css) => {
   return (<AiOutlineFundProjectionScreen css={css} />);
 }
 
+const SubContent = 
+  <ProjectsSubContent   
+    entries={PROJECTS.projects}
+  />;
+
 const Projects = () => {
 
   return (
@@ -29,7 +35,7 @@ const Projects = () => {
         <Card 
           title={PROJECTS.title} 
           subtitle={PROJECTS.subtitle}
-          subContent={undefined}
+          subContent={SubContent}
           iconFactory={iconFactory}
         />
     </Container>
