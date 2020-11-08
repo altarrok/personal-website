4bf79b96087b98fa86fa91557a116a74cb626b11
Altay Batuhan <45828884+altarrok@users.noreply.github.com>
2020-11-07 22:58:59 -0800

    styled 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,18 @@
 import React from "react"
+import Card from "../components/card"
+import Container from "../components/container"
 
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <div>
+  <div style={{backgroundColor: "grey"}}>
     <SEO title="404: Not found" />
+    <Container>
+      <Card
+        title={"404 Not Found"}
+        subContent={<p>The page you are looking for does not exist. <a href="https://www.altaybatuhan.com"> Go back to altaybatuhan.com</a></p>}
+      />
+    </Container>
   </div>
 )
 
