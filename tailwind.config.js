0ccf49dc6e0b0788534ed348fd7aac4ea6733e95
Altay Batuhan <altaybatuhanmail@gmail.com>
2023-02-14 09:59:39 -0800

    Big update to next.js, sorry for no commits, rushed :)

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        'l': '-0.7rem 0 2rem 0 rgb(21 27 31 / 50%)'
+      }
+    },
+  },
+  plugins: [],
+}
